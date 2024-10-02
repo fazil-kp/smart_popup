6797174eef30c1aaff2d24442389e0b1970b0a7c
Fazil <104255104+fazil-kp@users.noreply.github.com>
2024-10-02 22:43:39 +0530

    update 13 files

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -10,7 +10,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Custom Alert Popup Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Smart Popup Demo',
       theme: ThemeData(colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple), useMaterial3: true),
       home: const CustomAlertPopupExample(),
     );
@@ -24,31 +25,39 @@ class CustomAlertPopupExample extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            showDialog(
-              context: context,
-              builder: (context) => SmartPopup(
-                title: "Smart Popup",
-                subTitle: "This is a smart popup. It can display alerts and messages",
-                firstButtonText: "Ok",
-                secondButtonText: "Cancel",
-                firstButtonTap: () {
-                  Navigator.of(context).pop();
-                },
-                secondButtonTap: () {
-                  Navigator.of(context).pop();
-                },
-                // Do not pass image and video path 5simultaneously; pass one at a time.
-                imagePath: "assets/images/demo_image.png",
-                // videoPath: "assets/videos/demo_video.mp4",
-                // videoVolume: 100,
-                // videoPlayBackSpeed: 2.5,
-              ),
-            );
-          },
-          child: const Text("Tap Here"),
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        decoration: const BoxDecoration(color: Colors.white, image: DecorationImage(image: AssetImage("assets/images/bg_image.png"), fit: BoxFit.fill)),
+        child: Center(
+          child: ElevatedButton(
+            onPressed: () {
+              showDialog(
+                context: context,
+                builder: (context) => SmartPopup(
+                  title: "Smart Popup",
+                  subTitle: "This is a smart popup. It can display alerts and messages",
+                  // hideFirstButton: true,
+                  // showCloseButton: false,
+                  // showButtons: false,
+                  // firstButtonText: "Ok",
+                  secondButtonText: "Cancel",
+                  firstButtonTap: () {
+                    Navigator.of(context).pop();
+                  },
+                  secondButtonTap: () {
+                    Navigator.of(context).pop();
+                  },
+                  // Do not pass image and video path 5simultaneously; pass one at a time.
+                  imagePath: "assets/images/demo_image.png",
+                  // videoPath: "assets/videos/demo_video.mp4",
+                  // videoVolume: 100,
+                  // videoPlayBackSpeed: 2.5,
+                ),
+              );
+            },
+            child: const Text("Tap Here"),
+          ),
         ),
       ),
     );
