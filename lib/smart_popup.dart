b532a779808452617c9f786359189a2aea4cc78d
Fazil <104255104+fazil-kp@users.noreply.github.com>
2024-11-16 23:25:11 +0530

    update smart_popup.dart

--- lib/smart_popup.dart
+++ lib/smart_popup.dart
@@ -21,7 +21,7 @@ class SmartPopup extends HookWidget {
   final String? primaryButtonText;
   final String? secondaryButtonText;
   final String? imagePath;
-  final String? videoPath; // Commmmming sooooooon!
+  final String? videoPath; // commmmming sooooooon!
   final VoidCallback? primaryButtonTap;
   final VoidCallback? secondaryButtonTap;
   final int? timerDelay;
